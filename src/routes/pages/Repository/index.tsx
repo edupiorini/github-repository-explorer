e9d1eb45345ca29896a6913db5fea836c045ff43
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-12-14 06:03:49 -0300

    first commit

---
+++ src/routes/pages/Repository/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+    return <h1>Repository</h1>;
+}
+/**
+ * Poderiamos também utilizar a componentização em uma função,
+ * mas seria muito mais complicado para adicionar a tipagem, então
+ * utilizamos a constante com uma arrow functionality
+ * */
+
+ export default Repository;
