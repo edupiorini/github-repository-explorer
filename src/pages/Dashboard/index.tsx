0c1925c4ce72102f08feed797b9589400a619913
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-12-15 08:33:19 -0300

    using gihub api Ok

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,63 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/github-logo.svg';
+import api from '../../services/api';
+
+interface Repository {
+  // !!! INSIRA SOMENTE AS TIPAGENS QUE SERÃO USADAS!!!!!!!!
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    /**
+     * Adição de um novo repositorio
+     * Consumir API do Github
+     * Salvar o novo repositorio no estado de repositorios
+     */
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit"> Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/42455128?s=460&u=c73dd082df56232ef9cb3bdaa7459e221b866d9e&v=4"
-            alt="Eduardo Piorini"
-          />
-          <div>
-            <strong>edupiorini/conceitos-nodejs</strong>
-            <p>
-              Repositório criado para desenvolvimento do desafio sobre os
-              conceitos de Node.JS
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/42455128?s=460&u=c73dd082df56232ef9cb3bdaa7459e221b866d9e&v=4"
-            alt="Eduardo Piorini"
-          />
-          <div>
-            <strong>edupiorini/conceitos-nodejs</strong>
-            <p>
-              Repositório criado para desenvolvimento do desafio sobre os
-              conceitos de Node.JS
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/42455128?s=460&u=c73dd082df56232ef9cb3bdaa7459e221b866d9e&v=4"
-            alt="Eduardo Piorini"
-          />
-          <div>
-            <strong>edupiorini/conceitos-nodejs</strong>
-            <p>
-              Repositório criado para desenvolvimento do desafio sobre os
-              conceitos de Node.JS
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
