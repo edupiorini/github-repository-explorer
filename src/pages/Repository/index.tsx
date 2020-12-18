19734e3734fcd2b59056c9555149e12e139923c1
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-12-18 19:43:50 -0300

    repositories layout ok

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 import logoImg from '../../assets/github-logo.svg';
 
@@ -13,13 +13,51 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <Header>
-      <img src={logoImg} alt="GitHub Explorer" />
-      <Link to="/dashbord">
-        <FiChevronLeft size={16} />
-        Voltar
-      </Link>
-    </Header>
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/42455128?s=460&u=c73dd082df56232ef9cb3bdaa7459e221b866d9e&v=4"
+            alt="edupiorini"
+          />
+          <div>
+            <strong>edupiorini/conceitos-nodejs</strong>
+            <p>descrição do respositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="kkjhg">
+          <div>
+            <strong>kjdshfksjdfhksj</strong>
+            <p>ksjfkjsdfh</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 /**
