4c95995025a4606dcb078325a2d3a9aa1dcf8003
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-12-18 12:26:04 -0300

    begning of routes (visual)

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import { Header } from './styles';
+
+import logoImg from '../../assets/github-logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <Header>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Link to="/dashbord">
+        <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+    </Header>
+  );
 };
 /**
  * Poderiamos também utilizar a componentização em uma função,
